73c1967f7c0826f35c224184915122addd056bfc
MaximGolubtsov <maksim.golubtsov@mytona.com>
2021-03-03 13:54:21 +0300

    Init UE4 commit

---
+++ BE/Source/BE/BE.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BE, "BE" );
---
+++ BE/Source/BE/BEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BE_API ABEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
